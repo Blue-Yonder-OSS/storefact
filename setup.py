bcb2d70ea92a2265213cb44b0047346da902685f
Aditya Jaroli <adityajaroli@gmail.com>
2024-01-12 21:35:23 +0530

    Removed support for python 2.7

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.9",
